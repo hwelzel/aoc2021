da4db760db62583c6b7ee1c42ebc55906da08f2a
Henrique Welzel <henriquewelzel@gmail.com>
2021-12-01 22:47:44 +0000

    Use auto inputs instead of span
    Span is nice, but I don't need it in this case. Also, this now should
    work with any number type.

--- day1.cxx
+++ day1.cxx
@@ -3,19 +3,18 @@
 #include <range/v3/functional/comparisons.hpp>
 #include <range/v3/view/adjacent_filter.hpp>
 
-#include <span>
 #include <array>
 
 namespace {
 
-auto count_adj_increases(const std::span<const double> input) {
+auto count_adj_increases(const auto& input) {
     if (input.empty()) {
         return 0l;
     }
     return ranges::distance(ranges::views::adjacent_filter(input, ranges::less())) - 1;
 }
 
-auto test(const std::span<const double> input) {
+auto test(const auto& input) {
     fmt::print("testing: {{{}}}\n", fmt::join(input, ", "));
     fmt::print("measurements increased {} time(s)\n", count_adj_increases(input));
 }
@@ -23,6 +22,7 @@ auto test(const std::span<const double> input) {
 } // namespace
 
 int main() {
+    test(std::array{1, 2, 2, 3, 2});
     test(std::array{1.0, 2.0, 2.0, 3.0, 2.0});
     test(std::array{1.0});
     test(std::array<double, 0>{});
