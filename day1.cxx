b4d825a25535b74603908e3304615a32f93d40dc
Henrique Welzel <henriquewelzel@gmail.com>
2021-12-01 22:31:25 +0000

    Keep only one algorithm

--- day1.cxx
+++ day1.cxx
@@ -1,12 +1,7 @@
 #include <fmt/format.h>
 #include <range/v3/distance.hpp>
-#include <range/v3/to_container.hpp>
-#include <range/v3/functional/identity.hpp>
-#include <range/v3/algorithm/count_if.hpp>
-#include <range/v3/view/drop.hpp>
-#include <range/v3/view/transform.hpp>
+#include <range/v3/functional/comparisons.hpp>
 #include <range/v3/view/adjacent_filter.hpp>
-#include <range/v3/numeric/adjacent_difference.hpp>
 
 #include <span>
 #include <array>
@@ -14,14 +9,6 @@
 namespace {
 
 auto count_adj_increases(const std::span<const double> input) {
-    std::vector<double> diffs(input.size());
-    ranges::adjacent_difference(input, diffs);
-    const auto increases = diffs | ranges::views::drop(1)
-            | ranges::views::transform([](auto d) { return d > 0.0; });
-    return ranges::count_if(increases, ranges::identity());
-}
-
-auto count_adj_increases2(const std::span<const double> input) {
     if (input.empty()) {
         return 0l;
     }
@@ -31,7 +18,6 @@ auto count_adj_increases2(const std::span<const double> input) {
 auto test(const std::span<const double> input) {
     fmt::print("testing: {{{}}}\n", fmt::join(input, ", "));
     fmt::print("measurements increased {} time(s)\n", count_adj_increases(input));
-    fmt::print("measurements increased {} time(s)\n", count_adj_increases2(input));
 }
 
 } // namespace
